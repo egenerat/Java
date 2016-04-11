12d5e2146e0d803f37e884101153d45a83218829
Emile Generat <emile.generat@outlook.com>
2016-04-11 18:20:40 +0100

    java direct object creation

---
+++ dependency_injection/0_Java_DirectObjectCreation/App.java
@@ -0,0 +1,8 @@
+public class App {
+	private Logger log = new Logger();
+
+	public static void main(String... args) {
+		App app = new App();
+		app.log.write("hello");
+	}
+}
---
+++ dependency_injection/0_Java_DirectObjectCreation/Logger.java
@@ -0,0 +1,5 @@
+public class Logger {
+	public void write(String msg) {
+		System.out.println("Writing: " + msg);
+	}
+}
